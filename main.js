890daa1260fdb697521d7ff877a81f6848ab34ca
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-21 10:58:28 -0800

    --add cube mesh, scene, camera, and canvas

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as Three from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new Three.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+
+const canvas = document.querySelector('.webgl')
+
+const renderer = new Three.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new Three.BoxGeometry(1, 1, 1);
+const material = new Three.MeshBasicMaterial({
+  color: 'lightcoral'
+});
+const cube = new Three.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate()
